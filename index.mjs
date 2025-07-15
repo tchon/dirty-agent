c52b2dab2a7133904f7429d1c0da60b5fdc5a5d3
TIm Chon <devchon+github@sonic.net>
2025-07-14 19:00:21 -0700

    add parse_browser

--- index.mjs
+++ index.mjs
@@ -5,13 +5,31 @@ export function is_valid_browser_ua(user_agent) {
   return user_agent.startsWith('Mozilla/');
 }
 
-const OS_REGEX = /^[A-Z]\w+\W+\d+\.\d+(?:\.\d+)?\s+\((?:\w+;\s+(?:\w+;\s+)?)?(\w[^;\)]+)\s+(\d+(?:[_\.]\d[_\.\d]*)?)\b/;
+const REGEX = {
+  OS: /^[A-Z]\w+\W+\d+\.\d+(?:\.\d+)?\s+\((?:\w+;\s+(?:\w+;\s+)?)?(\w[^;\)]+)\s+(\d+(?:[_\.]\d[_\.\d]*)?)\b/,
+  SAFARI: /\b(?:Mac|iPhone|iPad).*\s+Version\/(\d+\.\d+(?:\.\d+)?)\s+((?:[A-Z]\w+\/\w+\s+)?Safari)/i,
+  FIREFOX: /\bGecko\W+(?:\d[\d\.]*\s+)?(F\w+)\/(\d[\.\d]+)\b/i,
+};
 
 export function parse_os(user_agent) {
   if (typeof user_agent === "string") {
-    const matches = user_agent.match(OS_REGEX)
+    const matches = user_agent.match(REGEX.OS);
     if (matches) {
       return { os: matches[1], os_version: matches[2] };
+    }
+  }
+  return { os: "unknown", os_version: "unknown" };
+}
+
+export function parse_browser(user_agent) {
+  if (typeof user_agent === "string") {
+    let matches = user_agent.match(REGEX.SAFARI);
+    if (matches) {
+      return { browser_name: matches[2], browser_version: matches[1] };
+    }
+    matches = user_agent.match(REGEX.FIREFOX);
+    if (matches) {
+      return { browser_name: matches[1], browser_version: matches[2] };
     }
   }
   return { os: "unknown", os_version: "unknown" };
--- index.test.mjs
+++ index.test.mjs
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { is_valid_browser_ua, parse_os } from "./index.mjs";
+import { is_valid_browser_ua, parse_os, parse_browser } from "./index.mjs";
 
 describe("is_valid_browser_ua", () => {
   it("should test if user-agent follows convention", () => {
@@ -27,6 +27,9 @@ const USER_AGENTS = {
   motorola_moto_edge: "Dalvik/2.1.0 (Linux; U; Android 17; moto edge 30 neo Build/AP3A.241105.008)",
   playstation: "Mozilla/5.0 (PlayStation 4 3.11) AppleWebKit/537.73 (KHTML, like Gecko)",
   xbox: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; Xbox; Xbox Series X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36 Edge/20.02",
+  firefox_desktop: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0",
+  firefox_iphone: "Mozilla/5.0 (iPhone; CPU iPhone OS 11_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/30.0 Mobile/15E148 Safari/605.1.15",
+  firefox_android: "Mozilla/5.0 (Android 11; Mobile; rv:68.0) Gecko/68.0 Firefox/84.0",
 };
 
 describe("parse_os", () => {
@@ -109,4 +112,104 @@ describe("parse_os", () => {
     assert.equal(out.os, "Windows NT");
     assert.equal(out.os_version, "10.0");
   });
+});
+
+
+describe("parse_browser", () => {
+  it("should parse Safari", () => {
+    const ua = USER_AGENTS.macbook;
+    const out = parse_browser(ua);
+
+    assert.equal(out.browser_name, "Safari");
+    assert.equal(out.browser_version, "18.4");
+  });
+
+  it("should parse Firefox on Desktop/Laptop", () => {
+    const ua = USER_AGENTS.firefox_desktop;
+    const out = parse_browser(ua);
+
+    assert.equal(out.browser_name, "Firefox");
+    assert.equal(out.browser_version, "84.0");
+  });
+
+  it("should parse Firefox on iPhone", () => {
+    const ua = USER_AGENTS.firefox_iphone;
+    const out = parse_browser(ua);
+
+    assert.equal(out.browser_name, "FxiOS");
+    assert.equal(out.browser_version, "30.0");
+  });
+
+  it("should parse Firefox on Android", () => {
+    const ua = USER_AGENTS.firefox_android;
+    const out = parse_browser(ua);
+
+    assert.equal(out.browser_name, "Firefox");
+    assert.equal(out.browser_version, "84.0");
+  });
+/*
+  it("should parse iPhone", () => {
+    const ua = USER_AGENTS.iphone
+    const out = parse_os(ua);
+
+    assert.equal(out.os, "CPU iPhone OS");
+    assert.equal(out.os_version, "18_5");
+  });
+
+  it("should parse iPad", () => {
+    const ua = USER_AGENTS.ipad;
+    const out = parse_os(ua);
+
+    assert.equal(out.os, "CPU iPhone OS");
+    assert.equal(out.os_version, "18_3");
+  });
+
+  it("should parse Huawei", () => {
+    const ua = USER_AGENTS.huawei;
+    const out = parse_os(ua);
+
+    assert.equal(out.os, "Android");
+    assert.equal(out.os_version, "12");
+  });
+
+  it("should parse Samsung", () => {
+    const ua = USER_AGENTS.samsung;
+    const out = parse_os(ua);
+
+    assert.equal(out.os, "Android");
+    assert.equal(out.os_version, "14");
+  });
+
+  it("should parse Pixel", () => {
+    const ua = USER_AGENTS.pixel;
+    const out = parse_os(ua);
+
+    assert.equal(out.os, "Android");
+    assert.equal(out.os_version, "14");
+  });
+
+  it("should parse Motorola Moto Edge", () => {
+    const ua = USER_AGENTS.motorola_moto_edge;
+    const out = parse_os(ua);
+
+    assert.equal(out.os, "Android");
+    assert.equal(out.os_version, "17");
+  });
+
+  it("should parse Playstation", () => {
+    const ua = USER_AGENTS.playstation;
+    const out = parse_os(ua);
+
+    assert.equal(out.os, "PlayStation 4");
+    assert.equal(out.os_version, "3.11");
+  });
+
+  it("should parse Xbox", () => {
+    const ua = USER_AGENTS.xbox;
+    const out = parse_os(ua);
+
+    assert.equal(out.os, "Windows NT");
+    assert.equal(out.os_version, "10.0");
+  });
+  */
 });
