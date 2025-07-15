7a07c9b9961f41e4e29cfef5aca82731fb32920b
TIm Chon <devchon+github@sonic.net>
2025-07-15 10:41:30 -0700

    no to esm naming for now

--- index.test.js
+++ index.test.js
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
-import { parse_os, parse_browser } from "./index.mjs";
+import { parse_os, parse_browser } from "./index.js";
 
 const USER_AGENTS = {
   windows: "Mozilla/5.0 (Windows NT 10.0; Trident/7.0; rv:11.0) like Gecko",
